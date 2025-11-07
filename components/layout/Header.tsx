0c8fe781dc37d1c11cb183c925e70bb17e0e3d73
jasmin <chjasmin27@gmail.com>
2025-11-07 21:08:29 +0600

    who we are page initially done without responsiveness

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -13,7 +13,6 @@ const Header = () => {
   return (
     <div className={`${poppins.className}`}>
       <Container>
-        {/* Top Section */}
         <section className="flex items-center justify-between mt-[33px] gap-6">
           <div className="shrink-0">
             <Image
@@ -33,18 +32,9 @@ const Header = () => {
             <Buttons />
           </div>
         </section>
-
-            {/* Bottom Section */}
-            <section className='flex items-center gap-50 mt-10'>
-                <div className='max-w-xl'>
-                    <h2 className='font-extrabold text-[51px]'>THE CHT <span className='text-[#FF951B]'>INDIGENOUS</span> WOMANIFESTO</h2>
-                </div>
-                <div>
-                    <p style={{ fontFamily: '"Book Antiqua",  serif' }} className='ml-15'>Empowering indigenous women and communities to rise against systemic oppression, reclaim their voices.</p>
-                </div>
-            </section>
-        </div>
-    );
+      </Container>
+    </div>
+  );
 };
 
 export default Header;
